1b2d61fde64f8cfeb328aade3250afb28ec6a6a3
Paul deBodinat <polo2bodi@gmail.com>
2018-01-13 19:06:26 +0100

    model elements destroyed

--- db/migrate/20171208131942_create_elements.rb
+++
@@ -1,9 +0,0 @@
-class CreateElements < ActiveRecord::Migration[5.0]
-  def change
-    create_table :elements do |t|
-      t.references :category, foreign_key: true
-      t.text :content
-      t.boolean :public
-    end
-  end
-end
