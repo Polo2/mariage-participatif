a2ef53ecf05e8054b5bcfca57ee349928c64cef5
Paul deBodinat <polo2bodi@gmail.com>
2018-01-17 12:20:50 +0100

    home-page with gradiant and form-questions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   private
 
   def skip_pundit?
-    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/
+    devise_controller? || params[:controller] =~ /(^(rails_)?admin)|(^pages$)/ || questions_controller?
   end
 
   def set_user_weddings
--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,12 +1,13 @@
 class QuestionsController < ApplicationController
 
+skip_before_action :authenticate_user!
 
   def create
     @question = Question.new(params.require(:question).permit(:name, :email, :message))
     if @question.save
-      redirect_to root_path
+      redirect_to root_path, notice: "Merci pour votre message"
     else
-      render "pages/home"
+      render "pages/home", error: "Votre message n'a pas pu nous arriver"
     end
   end
 
