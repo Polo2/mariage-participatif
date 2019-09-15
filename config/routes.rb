f6321af90fcf3503b810807ecdb35930f10d4f16
Paul M2B <polo2bodi@gmail.com>
2019-09-15 16:48:59 +0200

    Remove accomodation_request route
    Unnecessary since af5aacf.

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,11 @@
 Rails.application.routes.draw do
-
-
   devise_for :users
    # controllers: { omniauth_callbacks: 'users/omniauth_callbacks', registrations: 'registrations' }
-
 
   # scope '(:locale)', locale: /fr|en|it/ do
 
   root to: 'pages#home'
   get "users/profile", to: 'pages#profile'
-
 
   resources :weddings do
     get "tasks/upload", to: "tasks#upload"
@@ -25,15 +21,9 @@ Rails.application.routes.draw do
       resources :services, only: [:new, :create, :update, :destroy]
       resources :messages, only: [:create, :index, :new, :destroy, :show]
     end
-    resources :accomodation_requests, only: [ :index, :edit, :destroy ]
     resources :services, only: [:index]
   end
   resources :questions, only: [:new, :create]
 
   mount Attachinary::Engine => "/attachinary"
-  # end
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
-
